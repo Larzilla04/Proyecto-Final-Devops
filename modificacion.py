5aac844f5b902df4652c590ba20b9740e374dca7
Lars <al03109601@tecmilenio.mx>
2025-05-12 16:18:07 -0600

    Para modificar

---
+++ modificacion.py
@@ -0,0 +1,20 @@
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+tabla = dynamodb.Table('MiTabla')
+
+# Insertar
+tabla.put_item(Item={'ID': '001', 'Nombre': 'Carlos'})
+print("Insertado")
+
+# Modificar
+tabla.update_item(
+    Key={'ID': '001'},
+    UpdateExpression='SET Nombre = :val1',
+    ExpressionAttributeValues={':val1': 'Carlos A.'}
+)
+print("Modificado")
+
+# Eliminar
+tabla.delete_item(Key={'ID': '001'})
+print("Eliminado")
