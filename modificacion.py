2c7cfc05d2abf2e3242a59cf6ae2c6189101ba22
Lars <al03109601@tecmilenio.mx>
2025-05-12 16:30:21 -0600

    Actualización en script de carga a S3

--- modificacion.py
+++ modificacion.py
@@ -4,17 +4,17 @@ dynamodb = boto3.resource('dynamodb')
 tabla = dynamodb.Table('proyectoTabla')
 
 # Insertar
-tabla.put_item(Item={'ID': 'proyecto1', 'Nombre': 'Carlos'})
+tabla.put_item(Item={'proyecto1': '001', 'Nombre': 'Carlos'})
 print("Insertado")
 
 # Modificar
 tabla.update_item(
-    Key={'ID': '001'},
+    Key={'proyecto1': '001'},
     UpdateExpression='SET Nombre = :val1',
     ExpressionAttributeValues={':val1': 'Carlos A.'}
 )
 print("Modificado")
 
 # Eliminar
-tabla.delete_item(Key={'ID': '001'})
+tabla.delete_item(Key={'proyecto1': '001'})
 print("Eliminado")
